3560bf11e36da2f1dc7b8393449c86a63912e55a
steel-a <stell4b@gmail.com>
2020-06-06 19:46:13 -0300

    added tests

---
+++ tests.py
@@ -0,0 +1,33 @@
+from dbpy.db_mysql import DB
+from utilpy.config import Config
+
+db = DB(Config('getWebData.ini').config['connectionString'])
+db.exec('drop table if exists test')
+db.exec('create table test (a int, b varchar(10))')
+db.exec('insert into test values (1,"aaa")')
+db.exec('insert into test values (2,"bbb")')
+
+assert db.getValue('select b from test where a = 1') == 'aaa'
+assert db.getValue('select a from test where a = 1') == 1
+
+dic = db.getRowDic('select * from test where a=1')
+assert dic['a']==1
+assert dic['b']=='aaa'
+
+lst = db.getListRowsDic('select * from test')
+assert lst[0]['a']==1
+assert lst[0]['b']=='aaa'
+assert lst[1]['a']==2
+assert lst[1]['b']=='bbb'
+
+dic = db.getDicRowsList('select * from test')
+assert dic['a'][0]==1
+assert dic['a'][1]==2
+assert dic['b'][0]=='aaa'
+assert dic['b'][1]=='bbb'
+
+lst = db.fetchall('select * from test')
+assert lst[0][0]==1
+assert lst[0][1]=='aaa'
+assert lst[1][0]==2
+assert lst[1][1]=='bbb'
